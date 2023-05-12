378464aa309779a87dfa3da0acb7f5cca0b37a8d
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2023-05-13 05:01:42 +0600

    functionalities done

--- app.js
+++ app.js
@@ -1,8 +1,95 @@
-const pw = document.getElementById("pw");
-const copy = document.getElementById("copy");
-const len = document.getElementById("len");
-const upper = document.getElementById("upper");
-const lower = document.getElementById("lower");
-const number = document.getElementById("number");
-const symbol = document.getElementById("symbol");
-const generate = document.getElementById("generate");
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+=";
+
+
+function getLowercase() {
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+
+function getUppercase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+getUppercase();
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+
+// Generate password
+function generatePassword() {
+    const len = lenEl.value;
+    let password = "";
+
+    for (let i = 0; i < len; i++) {
+        const x = generateX();
+        password += x;
+    }
+
+    pw.innerText = password;
+}
+
+function generateX() {
+    const xs = [];
+
+    if (upperEl.checked) {
+        xs.push(getUppercase());
+    }
+
+    if (lowerEl.checked) {
+        xs.push(getLowercase());
+    }
+
+    if (numberEl.checked) {
+        xs.push(getNumber());
+    }
+
+    if (symbolEl.checked) {
+        xs.push(getSymbol());
+    }
+
+
+    if (xs.length === 0) return "";
+
+    return xs[Math.floor(Math.random() * xs.length)];
+}
+
+generateEl.addEventListener("click", generatePassword);
+
+
+
+// Copy password to clipboard
+function copyPassword() {  
+    const textarea = document.createElement("textarea");
+    const password = pw.innerText;
+
+    if (!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Password copied to clipboard!");
+}
+
+copyEl.addEventListener("click", copyPassword);
+
